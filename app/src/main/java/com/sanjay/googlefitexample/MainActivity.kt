2e8c143668a6f2f10d87f0570e21a4f3c020f541
sanjaydraws <sprajapat8331@gmail.com>
2021-10-08 23:14:30 +0530

    OAuth 2.0 set up

--- app/src/main/java/com/sanjay/googlefitexample/MainActivity.kt
+++ app/src/main/java/com/sanjay/googlefitexample/MainActivity.kt
@@ -2,10 +2,16 @@ package com.sanjay.googlefitexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sanjay.googlefitexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding?.apply {
+            setContentView(this.root)
+        }
+
     }
 }
